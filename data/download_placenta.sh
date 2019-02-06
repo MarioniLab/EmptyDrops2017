dfb4f6fbdf88ed35216c86c16cdba365e524d085
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2019-02-06 13:08:40 +0000

    Download origin file in script.

--- data/download_placenta.sh
+++ data/download_placenta.sh
@@ -1,4 +1,4 @@
-# get the data
+# Get the data
 wget https://jmlab-gitlab.cruk.cam.ac.uk/publications/EmptyDrops2017-DataFiles/raw/08a775d772aadd25404f141bf95fce79412a0f3b/placenta_vento-tormo.zip
 unzip placenta_vento-tormo.zip
 
@@ -9,3 +9,6 @@ do
     rm $x"_raw_gene_bc_matrices.tar.gz"
 done
 rm placenta_vento-tormo.zip
+
+# Download the fetal/maternal annotation.
+wget https://jmlab-gitlab.cruk.cam.ac.uk/publications/EmptyDrops2017-DataFiles/raw/master/cell_origin.csv
