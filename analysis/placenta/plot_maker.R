631ee3252ed6ecc37b7aaa47e44d5b64814a5dff
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2019-02-06 10:09:27 +0000

    Annotated a few more clusters in the placenta data.

--- analysis/placenta/plot_maker.R
+++ analysis/placenta/plot_maker.R
@@ -5,16 +5,11 @@ sce <- readRDS("sce.rds")
 coords <- reducedDim(sce, "TSNE")
 dir.create("pics", showWarning=FALSE)
 
-mo.clust <- metadata(sce)$Monocytes
-monocytes <- colMeans(coords[sce$Cluster==mo.clust,]) 
-
 # Defining arrow coordinates.
-FUN <- function(coloration, ...) {
+FUN <- function(coloration, loc, SHIFT, WIDTH, orientation=-5, ...) {
   plot(coords[,1], coords[,2], col=coloration, pch=16, xlab="t-SNE1", ylab="t-SNE2", cex.axis=1.2, cex.lab=1.4, ...)
-  SHIFT <- c(-11.5, -8)
-  WIDTH <- c(0, 4)
-  arrows(monocytes[1] - SHIFT[1] - WIDTH[1], monocytes[2] - SHIFT[2],
-         monocytes[1] - SHIFT[1] - 5, monocytes[2] - SHIFT[2] - WIDTH[2],
+  arrows(loc[1] + SHIFT[1] + WIDTH[1], loc[2] + SHIFT[2],
+         loc[1] + SHIFT[1] + orientation, loc[2] + SHIFT[2] + WIDTH[2],
          angle=20, length=0.1, lwd=2, col="black")
 }
 
@@ -27,27 +22,71 @@ COLBAR <- function(FUN, high.text="High", low.text="Low") {
   text(x+2.5, min(y), low.text, pos=1)
 }
 
+#######################################################
+# Monocyte arrows.
+
+mo.clust <- metadata(sce)$Monocytes
+monocytes <- colMeans(coords[sce$Cluster==mo.clust,]) 
+SHIFT <- c(11.5, 8)
+WIDTH <- c(0, -4)
+
 # Making a plot of detection status.
 coloration <- rep("grey", nrow(coords))
 coloration[sce$Detection=="EmptyDrops"] <- "salmon"
 coloration[sce$Detection=="CellRanger"] <- "dodgerblue"
 
 pdf("pics/by_detection.pdf")
-FUN(coloration)
+FUN(coloration, loc=monocytes, SHIFT=SHIFT, WIDTH=WIDTH)
 legend("bottomright", legend=c("Both", "EmptyDrops", "CellRanger"), col=c("grey", "salmon", "dodgerblue"), pch=16)
 dev.off()
 
-# Making a plot of KCNA5, STX11, CFP, and S100A12 expression.
+# Making a plot of Monocyte gene expression.
 library(viridis)
 
-by.gene <- Matrix::colSums(logcounts(sce)[c("KCNA5", "STX11", "CFP", "S100A12"),])
+by.gene <- sce$monocyte_genes_total 
 by.segment <- cut(by.gene, 100)
 coloration <- viridis(100)[by.segment]
 
 pdf("pics/by_monocyte.pdf")
 par(mar=c(5.1, 4.1, 4.1, 4.1), xpd=TRUE)
-FUN(coloration, main="Monocyte gene expression", cex.main=1.4)
+FUN(coloration, main="Monocyte gene expression", cex.main=1.4, loc=monocytes, SHIFT=SHIFT, WIDTH=WIDTH)
 COLBAR(viridis)
+dev.off()
+
+#######################################################
+# T cell arrows.
+
+t.clust <- metadata(sce)$TCells
+tcells <- colMeans(coords[sce$Cluster==t.clust,]) 
+SHIFT <- c(-10, 4)
+WIDTH <- c(0, -4)
+
+by.gene <- sce$t_genes_total 
+by.segment <- cut(by.gene, 100)
+coloration <- viridis(100)[by.segment]
+
+pdf("pics/by_tcell.pdf")
+par(mar=c(5.1, 4.1, 4.1, 4.1), xpd=TRUE)
+FUN(coloration, main="T cell gene expression", cex.main=1.4, loc=tcells, SHIFT=SHIFT, WIDTH=WIDTH, orientation=5)
+COLBAR(viridis)
+dev.off()
+
+#######################################################
+# Stripped nuclei arrows.
+
+stripped <- metadata(sce)$StrippedNucleus
+striploc <- colMeans(coords[sce$Cluster==stripped,]) 
+SHIFT <- c(-10, 4)
+WIDTH <- c(0, 0)
+
+by.gene <- sce$mito_genes_total 
+by.segment <- cut(by.gene, 100)
+coloration <- plasma(100)[by.segment]
+
+pdf("pics/by_mito.pdf")
+par(mar=c(5.1, 4.1, 4.1, 4.1), xpd=TRUE)
+FUN(coloration, main="Mitochondrial gene expression", cex.main=1.4, loc=striploc, SHIFT=SHIFT, WIDTH=WIDTH, orientation=5)
+COLBAR(plasma)
 dev.off()
 
 # Making a plot of ribosomal gene expression.
@@ -58,18 +97,8 @@ coloration <- inferno(100)[by.segment]
 
 pdf("pics/by_ribo.pdf")
 par(mar=c(5.1, 4.1, 4.1, 4.1), xpd=TRUE)
-FUN(coloration, main="Ribosomal protein expression", cex.main=1.4)
+FUN(coloration, main="Ribosomal protein expression", cex.main=1.4, loc=striploc, SHIFT=SHIFT, WIDTH=WIDTH)
 COLBAR(inferno)
 dev.off()
 
-# Making a plot of mitochondrial gene expression.
-by.gene <- sce$pct_counts_Mito
-by.gene <- pmin(by.gene, 10)
-by.segment <- cut(by.gene, 100)
-coloration <- plasma(100)[by.segment]
 
-pdf("pics/by_mito.pdf")
-par(mar=c(5.1, 4.1, 4.1, 4.1), xpd=TRUE)
-FUN(coloration, main="Mitochondrial proportion", cex.main=1.4)
-COLBAR(plasma, high.text=expression("" >= "10%"), low.text="0%")
-dev.off()
